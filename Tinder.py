7cea0a2babb36c1072108c2c31d5e165a595b488
Parth <parthks@umich.edu>
2021-01-13 11:08:04 +0530

    working...

---
+++ Tinder.py
@@ -0,0 +1,109 @@
+import requests
+
+
+import datetime
+from time import time
+
+
+from geopy.geocoders import Nominatim
+
+TOKEN = "c6f7b8ba-6edb-4318-b57d-ad2eef3d468e"
+TINDER_URL = "https://api.gotinder.com"
+geolocator = Nominatim(user_agent="auto-tinder")
+
+
+
+class tinderAPI():
+
+    def __init__(self):
+        self._token = TOKEN
+
+    def profile(self):
+        data = requests.get(TINDER_URL + "/v2/profile?include=account%2Cuser", headers={"X-Auth-Token": self._token}).json()
+        return Profile(data["data"], self)
+
+    def matches(self, limit=10):
+        data = requests.get(TINDER_URL + f"/v2/matches?count={limit}", headers={"X-Auth-Token": self._token}).json()
+        return list(map(lambda match: Person(match["person"], self), data["data"]["matches"]))
+
+    def like(self, user_id):
+        data = requests.get(TINDER_URL + f"/like/{user_id}", headers={"X-Auth-Token": self._token}).json()
+        return {
+            "is_match": data["match"],
+            "liked_remaining": data["likes_remaining"]
+        }
+
+    def dislike(self, user_id):
+        requests.get(TINDER_URL + f"/pass/{user_id}", headers={"X-Auth-Token": self._token}).json()
+        return True
+
+    def nearby_persons(self):
+        data = requests.get(TINDER_URL + "/v2/recs/core", headers={"X-Auth-Token": self._token}).json()
+        return list(map(lambda user: Person(user, self), data["data"]["results"]))
+
+
+
+class Person(object):
+
+    def __init__(self, user, api):
+
+        data = user["user"]
+        if (user.get("experiment_info", False)):
+            data["experiment_info"] = user["experiment_info"]
+
+        self._api = api
+        self.data = data
+
+        self.id = data["_id"]
+        self.name = data.get("name", "Unknown")
+
+        self.bio = data.get("bio", "")
+        self.distance = data.get("distance_mi", 0) / 1.60934
+
+        self.birth_date = datetime.datetime.strptime(data["birth_date"], '%Y-%m-%dT%H:%M:%S.%fZ') if data.get(
+            "birth_date", False) else None
+        self.gender = ["Male", "Female", "Unknown"][data.get("gender", 2)]
+
+        self.images = list(map(lambda photo: photo["url"], data.get("photos", [])))
+
+        self.jobs = list(
+            map(lambda job: {"title": job.get("title", {}).get("name"), "company": job.get("company", {}).get("name")}, data.get("jobs", [])))
+        self.schools = list(map(lambda school: school["name"], data.get("schools", [])))
+
+        if data.get("pos", False):
+            self.location = geolocator.reverse(f'{data["pos"]["lat"]}, {data["pos"]["lon"]}')
+
+        if (data.get("experiment_info", False) and 
+            data["experiment_info"].get("user_interests", False) and
+            data["experiment_info"]["user_interests"].get("selected_interests", False)):
+            interests = data["experiment_info"]["user_interests"]["selected_interests"]
+            self.interests = list(map(lambda d: d.get("name", ''), interests))
+
+
+    def __repr__(self):
+        return f"{self.id}  -  {self.name} ({self.birth_date.strftime('%d.%m.%Y')})"
+
+
+    def like(self):
+        return self._api.like(self.id)
+
+    def dislike(self):
+        return self._api.dislike(self.id)
+
+
+
+
+class Profile(Person):
+
+    def __init__(self, data, api):
+
+        super().__init__(data["user"], api)
+
+        self.email = data["account"].get("email")
+        self.phone_number = data["account"].get("account_phone_number")
+
+        self.age_min = data["user"]["age_filter_min"]
+        self.age_max = data["user"]["age_filter_max"]
+
+        self.max_distance = data["user"]["distance_filter"]
+        self.gender_filter = ["Male", "Female"][data["user"]["gender_filter"]]
---
+++ go.py
@@ -0,0 +1,103 @@
+import Tinder
+import time
+import random
+
+import json
+
+
+api = Tinder.tinderAPI()
+
+f=open("data.txt", "r+")
+ALL_DATA = json.load(f)
+f.close()
+
+f=open("swipelog.txt", "r+")
+SWIPE_LOG = json.load(f)
+f.close()
+
+
+SWIPE_COUNT = len(SWIPE_LOG)
+LIKED_COUNT = len(list(filter(lambda x: "LIKE" == list(x.values())[0], SWIPE_LOG)))
+
+print(SWIPE_COUNT, LIKED_COUNT)
+
+
+INTERESTS = ["Wine", "Craft Beer", "Grab a drink", "House Parties", "Dancing", "Festivals", "Travel", "Spirituality", "Movies"]
+KEYWORDS = ["wine", "go out", "party", "beer", "chill", "420", "dance", "dancing", "mango", "cheese", "travel", "explore", "software", "code", "coding", "geek", "ai", "singularity", "artificial intelligence", "edm", "psychedelic", "rave", "festival", "concert"]
+
+def chillz(data: Tinder.Person):
+    if hasattr(data, 'interests'):
+        for myInterest in INTERESTS:
+            if (myInterest in data.interests):
+                print("Intrested in " + myInterest)
+                return True
+    
+    bio = data.bio.lower()
+    for keyword in KEYWORDS:
+        if (bio.find(keyword) != -1):
+            print("Found '"+keyword+"' keyword in bio")
+            return True
+
+    return False
+
+
+
+
+
+
+def log(data: Tinder.Person, actionTaken):
+    global ALL_DATA, SWIPE_LOG
+
+    f=open("swipes.txt", "a+")
+    f.write(actionTaken + " - " + data.id + "\n")
+    f.write(data.bio + "\n")
+    if (hasattr(data, 'interests')):
+        f.write(str(data.interests) + "\n")
+    f.write("\n")
+    f.close()
+
+    
+    SWIPE_LOG.append({
+        data.id: actionTaken
+    })
+    f=open("swipelog.txt", "w+")
+    json.dump(SWIPE_LOG, f, indent=2)
+    f.close()
+
+    
+    ALL_DATA[data.id] = data.data
+    f=open("data.txt", "w+")
+    json.dump(ALL_DATA, f, indent=4)
+    f.close()
+
+
+
+if __name__ == "__main__":
+    
+    while True:
+        try:
+            potentials = api.nearby_persons()
+        except Exception as e:
+            print(e)
+            t = random.randint(300,600)
+            print("Taking a break...", t)
+            time.sleep(t)
+            continue
+
+
+        for girl in potentials:
+            if (chillz(girl)):
+                api.like(girl.id)
+                log(girl, "LIKE")
+                LIKED_COUNT += 1
+            else:
+                api.dislike(girl.id)
+                log(girl, "PASS")
+            SWIPE_COUNT += 1
+            print("Liked", LIKED_COUNT, "/", SWIPE_COUNT)
+
+            time.sleep(random.randint(5,30))
+
+
+
+
